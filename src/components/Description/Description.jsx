a894d9a6c4e113dd63195ed6788c7aa05c91e11a
David-McCaig <davidmccaig1@gmail.com>
2022-11-05 12:44:31 -0400

    Add description component

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -9,31 +9,37 @@ function Description() {
             <div className="description-output">
                 <div className="description-output__container">
                     <div className="description-output__author">
-                    <p>By Red Crow</p>
+                        <p>By Red Crow</p>
                     </div>
-                    <img className="description-output__icon" alt="Views icon" src={ViewIcon}></img>
-                    <p className="description-output__views">1,001,023</p>
+                    <p className="description-output__date">07/11/2021</p>
                 </div>
                    
                 <div className="description-output__container">
-                    <div className="description-output__date">
-                    <p>07/11/2021</p>
+                    <div className="description-output__views">
+                        <img className="description-output__icon" alt="Views icon" src={ViewIcon}></img>
+                        <p>1,001,023</p>
                     </div>
-                    <img className="description-output__icon" alt="Views icon" src={HeartIcon}></img>
-                    <p className="description-output__views">110,985</p>
+                    <div className="description-output__likes">
+                        <img className="description-output__icon" alt="Views icon" src={HeartIcon}></img>
+                        <p>110,985</p>
+                    </div>
                 </div>
-
+            </div>
                 <div className="summary">
                     <p className="summary__text">
                     On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible
 on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature
 only allowed for one full run before the conditions made it impossible to ride, that was all that was needed
-for event veteran
+for event veteran veteran
+Kyle Strait, who won the event for the second
+time -- eight years after his first Red Cow
+Rampage title
+
                     </p>
 
                 </div>
 
-            </div>
+            
         </section>
     );
 }
