64e818c4d80b607abc8b1556bc82ba93064f6927
David-McCaig <davidmccaig1@gmail.com>
2022-11-13 22:04:27 -0500

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UploadPage from "./pages/HomePage/UploadPage/UploadPage";
 import HomePage from "./pages/HomePage/HomePage/HomePage";
+import NotFound from "./pages/NotFound"
 // import { BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/:videoId" element={<HomePage />} />
           <Route path="/upload" element={<UploadPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
