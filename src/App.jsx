3cf999277b89debf1d23c1b24ed895d042493404
David-McCaig <davidmccaig1@gmail.com>
2022-11-13 19:32:02 -0500

    Add routes to video details page and main video. Add unique URL for each video. Connect API

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
-// import Header from "./pages/HomePage/Header/Header";
-// import Description from "./pages/HomePage/Description/Description";
-import VideoList from "./pages/HomePage/VideoList/VideoList";
-// import Comments from "./pages/HomePage/Comments/Comments";
 import './App.scss';
-import videos from "./data/videos.json";
-import videoDetails from "./data/video-details.json"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UploadPage from "./pages/HomePage/UploadPage/UploadPage";
 import HomePage from "./pages/HomePage/HomePage/HomePage";
-import VideoDetailsPage from "./pages/HomePage/VideoDetailsPage/VideoDetailsPage";
 // import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [selectedVideo, setSelectedVideos] = useState(videoDetails[0]);
 
-  const handleVideoChange = (videoId) => {
-    const newSelectedVideo = videoDetails.find((videos) => videoId === videos.id);
-    setSelectedVideos(newSelectedVideo);
-  }
-
-  const nonSelectedVideos = videos.filter((video) => {
-    return video.id !== selectedVideo.id;
-  });
+  
+  
 
   return (
     <>
       <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route  path="/" element={<HomePage selectedVideo={selectedVideo} videos={nonSelectedVideos} handleVideoChange={handleVideoChange} />} />
-        <Route path="/:id" element={<VideoDetailsPage/>} />
+        <Route  path="/" element={<HomePage/>} />
+        <Route path="/:videoId" element={<HomePage/>} />
         <Route path="/upload" element={<UploadPage />} />
       </Routes>
       </BrowserRouter>
