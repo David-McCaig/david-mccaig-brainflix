52946edf6014849fe7e86d5cd22fbf51b62ee64e
David-McCaig <davidmccaig1@gmail.com>
2023-06-18 14:40:41 +1200

    no changes made to files

--- src/components/VideosItem/VideosItem.jsx
+++ src/components/VideosItem/VideosItem.jsx
@@ -9,7 +9,10 @@ function VideosItem({ video }) {
     <Link to={"/" + video.id} style={{ textDecoration: "none" }} >
       <div className="videos__container">
         <div className="videos__wrapper">
-          <img className="videos__img" src={video.image} alt="above houses"></img>
+          <img className="videos__img" 
+          src={video.image}  
+          alt="above houses">
+          </img>
         </div>
         <div className="videos__text">
           <h3 className="videos__subtitle">{video.title}</h3>
